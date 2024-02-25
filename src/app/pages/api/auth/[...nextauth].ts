78d121d80ad7edb323b8ec6fe472b588bfa5aecd
Ivan Malakhovskyi <ivanmalahovskij121@gmail.com>
2024-02-25 12:18:28 +0200

    commented deprecated code

--- src/app/pages/api/auth/[...nextauth].ts
+++ src/app/pages/api/auth/[...nextauth].ts
@@ -1,41 +1,41 @@
-import NextAuth from "next-auth/next";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../../firebase";
+// import NextAuth from "next-auth/next";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import { signInWithEmailAndPassword } from "firebase/auth";
+// import { auth } from "../../../firebase";
 
-// //! Authorisaton with Email Firebase
-export const authOptions = {
-  // Configure one or more authentication providers
-  pages: {
-    signIn: "/signin",
-  },
+// // //! Authorisaton with Email Firebase
+// export const authOptions = {
+//   // Configure one or more authentication providers
+//   pages: {
+//     signIn: "/signin",
+//   },
 
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: {
-          label: "Username: ",
-          type: "text",
-          placeholder: "Some User 🤣",
-        },
-      },
-      async authorize(credentials): Promise<any> {
-        return await signInWithEmailAndPassword(
-          auth,
-          (credentials as any).email || "",
-          (credentials as any).password || ""
-        )
-          .then((userCredencials) => {
-            if (userCredencials.user) {
-              return userCredencials.user;
-            }
-            return null;
-          })
-          .catch((err) => console.log(err.message));
-      },
-    }),
-  ],
-};
+//   providers: [
+//     CredentialsProvider({
+//       name: "Credentials",
+//       credentials: {
+//         username: {
+//           label: "Username: ",
+//           type: "text",
+//           placeholder: "Some User 🤣",
+//         },
+//       },
+//       async authorize(credentials): Promise<any> {
+//         return await signInWithEmailAndPassword(
+//           auth,
+//           (credentials as any).email || "",
+//           (credentials as any).password || ""
+//         )
+//           .then((userCredencials) => {
+//             if (userCredencials.user) {
+//               return userCredencials.user;
+//             }
+//             return null;
+//           })
+//           .catch((err) => console.log(err.message));
+//       },
+//     }),
+//   ],
+// };
 
-export default NextAuth(authOptions);
+// export default NextAuth(authOptions);
